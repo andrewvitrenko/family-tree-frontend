de93a3b1ee83b21f047e0df76c6b6b49273b7850
andrewvitrenko <avitrenko2004@gmail.com>
2024-06-08 00:45:33 +0200

    feat(auth): - moved auth to custom hook - removed exhaustive code

--- src/components/pages/login/components/form/form.tsx
+++ src/components/pages/login/components/form/form.tsx
@@ -1,44 +1,23 @@
 'use client';
 
 import { Box } from '@mui/material';
-import { useRouter } from 'next/navigation';
 import { FC, memo, useCallback } from 'react';
 import { FormProvider, useForm } from 'react-hook-form';
-import { useMutation } from 'react-query';
 
-import { Auth } from '@/api/auth';
 import { Button, Input, PasswordInput } from '@/components/ui';
 import { PASSWORD_REGEX } from '@/constants/validation';
-import { useToast } from '@/hooks/use-toast';
-import { LocalStorage } from '@/services';
-import { TLoginPayload } from '@/types/api/auth';
-import { ELocalStorageKey } from '@/types/local-storage';
-import { ERoute } from '@/types/routes';
+import { useAuth } from '@/hooks/use-auth';
 
 import { TLoginForm } from '../../types/form';
 import * as styles from './styles';
 
 const Form: FC = () => {
-  const toast = useToast();
-  const router = useRouter();
-
-  const { mutate, isLoading } = useMutation({
-    mutationKey: ['login'],
-    mutationFn: (payload: TLoginPayload) => Auth.login(payload),
-    onError: (err) => toast.error((err as Error).message),
-    onSuccess: ({ access_token }) => {
-      LocalStorage.set(ELocalStorageKey.ACCESS_TOKEN, access_token);
-      router.push(ERoute.HOME);
-    },
-  });
+  const { login, isLoggingIn } = useAuth();
 
   const methods = useForm<TLoginForm>({ reValidateMode: 'onBlur' });
   const { formState, handleSubmit } = methods;
 
-  const onSubmit = useCallback(
-    (values: TLoginForm) => mutate(values),
-    [mutate],
-  );
+  const onSubmit = useCallback((values: TLoginForm) => login(values), [login]);
 
   return (
     <FormProvider {...methods}>
@@ -61,7 +40,7 @@ const Form: FC = () => {
           <Button
             disabled={!formState.isValid}
             type="submit"
-            loading={isLoading}
+            loading={isLoggingIn}
             sx={styles.button}
           >
             Login
